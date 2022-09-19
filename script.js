d4178d1d1a49659c5b766815c70d494bbd57eefc
Nelson Uprety <nelsonuprety3@gmail.com>
2022-09-19 20:47:31 +0100

    api data loaded

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const dice = document.getElementById('dice');
+
+const adviceContainer = document.querySelector('#box');
+
+const renderAdvice = function (data) {
+  const html = `
+    <h1 class="heading">ADVICE <span class='hash' id="hash">#${data.slip.id}</span></h1>
+      <p class="advice" id="advice">'${data.slip.advice}' </p>
+      <img src="/images/pattern-divider-mobile.svg" alt="divider" class="divider">
+    `;
+  adviceContainer.innerHTML = html;
+};
+
+async function adviceGenerator() {
+  const res = await fetch('https://api.adviceslip.com/advice');
+
+  const data = await res.json();
+  renderAdvice(data);
+  console.log(data.slip.advice);
+  console.log(data.slip.id);
+}
+
+dice.addEventListener('click', adviceGenerator());
+
+// adviceGenerator();
